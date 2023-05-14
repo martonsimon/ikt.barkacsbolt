2728bff231bf5ca66df06c058c712cc276e36721
KMen1 <oli70963@gmail.com>
2023-05-14 16:06:10 +0200

    Cleanup

--- scripts/products_loader.js
+++ scripts/products_loader.js
@@ -5,14 +5,14 @@ function createProductHTML(item) {
         <div class="col-12">
         <a href="pages/product.html?item=${
           item.id
-        }" class="noformat w-100 d-block"><div class="product-card rounded">
-          <div class="img">
+        }" class="noformat w-100 d-block"><div class="product-card d-flex gap-2 rounded">
+          <div class="img rounded">
             <img src="${item.photo_url}" alt="" class="" />
           </div>
-          <div class="info">
+          <div class="info d-flex flex-column justify-content-between">
             <div>
               <h3 class="fw-bold">${item.name}</h3>
-              <div class="rating">
+              <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16" > <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/> </svg>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16" > <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/> </svg>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16" > <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/> </svg>
@@ -21,10 +21,12 @@ function createProductHTML(item) {
               </div>
             </div>
             <div>
-              <span class="price">${toHUF(item.price)}</span>
-              <span class="unit-price">${toHUF(item.unit_price)}/${
-    item.unit
-  }</span>
+              <span class="mt-3 fw-bold lh-1 d-block fs-6 price">${toHUF(
+                item.price
+              )}</span>
+              <span class=" d-block text-muted mb-1 unit-price">${toHUF(
+                item.unit_price
+              )}/${item.unit}</span>
             </div>
           </div>
         </div></a>
